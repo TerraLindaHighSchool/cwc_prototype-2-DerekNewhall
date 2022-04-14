e0e52c9166b5c08cd95fd7cd3cb1b57104f2a025
DerekNewhall <89489562+DerekNewhall@users.noreply.github.com>
2022-04-14 15:14:00 -0700

    2.2.5 done

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -25,6 +25,10 @@ public class PlayerController : MonoBehaviour
         {
             transform.position = new Vector3(xRange, transform.position.y, transform.position.z);
         }
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            Instantiate(projectilePrefab, transform.position, projectilePrefab.transform.rotation);
+        }
         transform.Translate(Vector3.right * horizontalInput * Time.deltaTime * speed);
         horizontalInput = Input.GetAxis("Horizontal");
 
